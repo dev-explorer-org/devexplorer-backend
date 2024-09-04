66158aa96d1ea8a9112edca1e27f59b1ed2da88a
Valdemir Filho <filhux@gmail.com>
2024-09-04 20:38:38 -0300

    add healthcheck endpoint

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const server = express()
+
+server.get("/healthz", (request, response) => {
+  return response.status(200).send("OK")
+})
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+  console.log(`Server on: http://localhost:${PORT}`)
+})
