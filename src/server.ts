ba110158c1c6aea291461d38e796f7b4fab2ca23
Valdemir Filho <filhux@gmail.com>
2024-09-09 17:14:47 -0300

    config tsc build use esm-module

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express"
+import express from 'express'
 
 const server = express()
 
-server.get("/healthz", (request, response) => {
-  return response.status(200).send("OK")
+server.get('/healthz', (request, response) => {
+  return response.status(200).send('OK')
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
   console.log(`Server on: http://localhost:${PORT}`)
