813346f59699baf67fa267d1f0ee73795c74eab8
Valdemir Filho <filhux@gmail.com>
2024-09-18 16:52:11 -0300

    add eslint plugin imports sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+import tseslint from 'typescript-eslint'
 
 export default [
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,11 @@ server.get('/healthz', (request, response) => {
   return response.status(200).send('OK')
 })
 
+server.post('/teste', (request, response) => {
+  console.log('teste')
+  return response.redirect('http://localhost:3000/logado.html')
+})
+
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
